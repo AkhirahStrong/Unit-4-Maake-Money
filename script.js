f3b72a791e42a24172bffa161403d80c79c34514
Akhirahstrong <codeclubdetroit@gmail.com>
2023-05-23 10:19:31 -0400

    Add comments

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ coinForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   // Get data from form inputs
+  // Note: parsInt converts a String into an int
+  // .value adds the input
   const count = parseInt(document.getElementById("countInput").value);
   const coinType = document.getElementById("coinType").value;
 
   // Create specified coins and append to the coin container
   for (let i = 0; i < count; i++) {
+    // Display coin via span child
     const coin = document.createElement("span");
     coin.textContent = coinType;
     coin.className = "coin";
     coin.addEventListener("click", function () {
-      // Remove clicked coin
+      // Remove clicked coin via span child
       coinContainer.removeChild(coin);
     });
     coinContainer.appendChild(coin);
